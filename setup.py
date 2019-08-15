37ac5f94eb97d7eccfce56335b3d8c06ec18a379
Akhil Kondepudi <akhil@Akhils-iMac.local>
2019-08-15 11:00:39 -0500

    changed desc

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
-    description='A lightweight (5 KB) Python client to retrieve NBA data from stats.nba.com',  # Optional
+    description='A lightweight (6 KB), dynamic Python client to pull NBA data from stats.nba.com.',  # Optional
 
     # This is an optional longer description of your project that represents
     # the body of text which users will see when they visit PyPI.
